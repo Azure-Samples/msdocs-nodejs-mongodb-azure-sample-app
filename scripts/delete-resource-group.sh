129ac5c1b2a92a811747890b71fc8c302b509339
David Berry <dberry83702@yahoo.com>
2022-01-31 10:06:48 -0600

    Removed scripts directory and all CLI scripts as those have moved to a different repo

--- scripts/delete-resource-group.sh
+++
@@ -1,6 +0,0 @@
-RESOURCE_GROUP_NAME='msdocs-expressjs-mongodb-tutorial'
-
-
-# Removing a resource group will delete all Azure resources inside the resource group!
-az group delete \
-    --name $RESOURCE_GROUP_NAME
--- scripts/get-cosmos-db-connection-string.sh
+++
@@ -1,9 +0,0 @@
-# Gets the connection string for the database
-#   To connect from a tool like MongoDB Compass, this is what you need. 
-
-az cosmosdb keys list \
-    --type connection-strings \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --name $COSMOS_ACCOUNT_NAME \
-    --query "connectionStrings[?description=='Primary MongoDB Connection String'].connectionString" \
-    --output tsv
