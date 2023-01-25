8dd14c875d2d53a954cdc710f5ddb9b0866f1a15
Segun Tuase <66376866+Drizzy-hub@users.noreply.github.com>
2023-01-25 15:09:59 +0100

    api-doc

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-require("./middleware/refundsCron");
-require("./middleware/withdrawalCron");
-var createError = require("http-errors");
-var express = require("express");
-var mongoose = require("mongoose");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-const { format } = require("date-fns");
-const clientRoutes = require("./routes/auth");
-const uploadRoutes = require("./routes/upload");
-const paymentRoutes = require("./routes/payment");
-const refundRoutes = require("./routes/refund");
-const pickUpload = require("./routes/pickUpload");
-const account = require("./routes/withdrawal");
-const profile = require("./routes/profile");
-const writingTask = require("./routes/writingTask");
+require('./middleware/refundsCron');
+require('./middleware/withdrawalCron');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+var createError = require('http-errors');
+var express = require('express');
+var mongoose = require('mongoose');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const { format } = require('date-fns');
+const clientRoutes = require('./routes/auth');
+const uploadRoutes = require('./routes/upload');
+const paymentRoutes = require('./routes/payment');
+const refundRoutes = require('./routes/refund');
+const pickUpload = require('./routes/pickUpload');
+const account = require('./routes/withdrawal');
+const profile = require('./routes/profile');
+const writingTask = require('./routes/writingTask');
 
 // 1st party dependencies
-var configData = require("./config/connection");
+var configData = require('./config/connection');
 
 async function getApp() {
   // Database
@@ -26,38 +28,66 @@ async function getApp() {
 
   var app = express();
 
-  var port = normalizePort(process.env.PORT || "3000");
-  app.set("port", port);
+  var port = normalizePort(process.env.PORT || '3000');
+  app.set('port', port);
+
+  // Extended: https://swagger.io/specification/#infoObject
+  const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        version: '1.0.0',
+        title: 'Grandeur API',
+        description: ' API Information',
+        contact: {
+          name: 'Grandeur Team',
+        },
+        servers: ['https://grandeur-api.azurewebsites.net'],
+      },
+    },
+    // ['.routes/*.js']
+    apis: ['app.js'],
+  };
+  const swaggerDocs = swaggerJsDoc(swaggerOptions);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
   // view engine setup
-  app.set("views", path.join(__dirname, "views"));
-  app.set("view engine", "pug");
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'pug');
 
-  app.use(logger("dev"));
+  app.use(logger('dev'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.static(path.join(__dirname, 'public')));
 
   app.locals.format = format;
 
-  app.get("/", async (req, res) => {
-    res.json({ message: "Please visit /users to view all the users" });
+  app.get('/', async (req, res) => {
+    res.json({ message: 'Please visit /users to view all the users' });
   });
-  app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); // redirect bootstrap JS
+  app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
   app.use(
-    "/css",
-    express.static(__dirname + "/node_modules/bootstrap/dist/css")
+    '/css',
+    express.static(__dirname + '/node_modules/bootstrap/dist/css')
   ); // redirect CSS bootstrap
-
-  app.use("/users", clientRoutes);
-  app.use("/uploads", uploadRoutes);
-  app.use("/payments", paymentRoutes);
-  app.use("/refunds", refundRoutes);
-  app.use("/pick-upload", pickUpload);
-  app.use("/account", account);
-  app.use("/profile", profile);
-  app.use("/writing-task", writingTask);
+//Routes
+/**
+ * @swagger
+ * /users:
+ *  get:
+ *    description: Use to request all users
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+  app.use('/users', clientRoutes);
+  app.use('/uploads', uploadRoutes);
+  app.use('/payments', paymentRoutes);
+  app.use('/refunds', refundRoutes);
+  app.use('/pick-upload', pickUpload);
+  app.use('/account', account);
+  app.use('/profile', profile);
+  app.use('/writing-task', writingTask);
 
   // catch 404 and forward to error handler
   app.use(function (req, res, next) {
@@ -68,11 +98,11 @@ async function getApp() {
   app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render("error");
+    res.render('error');
   });
 
   return app;
